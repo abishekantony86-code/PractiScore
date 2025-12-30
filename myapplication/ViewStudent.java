35f3c1bd142dc734ba99d289747bd59683f8bf28
abishekantony86-code <abishekantony86@gmail.com>
2025-12-30 17:19:30 +0530

    Update ViewStudent.java

--- myapplication/ViewStudent.java
+++ myapplication/ViewStudent.java
@@ -6,7 +6,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
+ 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
